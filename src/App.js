2e59f91cdd6bdada6a9b1ea1b946c2cbf80ad910
Arpit Sirasao <arpitsirasao98@gmail.com>
2020-10-29 12:43:51 +0530

    for re deploying

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
               document.getElementById(home1").scrollIntoView();
             }}
           /> */}
-          <Route exact path="/" component={Container}
+          <Route path="/" component={Container}
             onEnter={function(){
               document.getElementById("home").scrollIntoView();
             }}
