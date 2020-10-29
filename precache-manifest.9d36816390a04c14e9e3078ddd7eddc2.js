7f1ffcff1445ea583176de2101c60a89bdaa9653
Arpit Sirasao <arpitsirasao98@gmail.com>
2020-10-29 12:15:59 +0530

    Updates

---
+++ precache-manifest.9d36816390a04c14e9e3078ddd7eddc2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25cc6a6fd0de1800f967d4898d040560",
+    "url": "/DigitalPortfolio/index.html"
+  },
+  {
+    "revision": "85d331bc3b5e0db6cdad",
+    "url": "/DigitalPortfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "465b9151d7051242417e",
+    "url": "/DigitalPortfolio/static/css/main.b4cc40e8.chunk.css"
+  },
+  {
+    "revision": "85d331bc3b5e0db6cdad",
+    "url": "/DigitalPortfolio/static/js/2.ee02e522.chunk.js"
+  },
+  {
+    "revision": "e92311d493a1dde02c3c18735aca81e5",
+    "url": "/DigitalPortfolio/static/js/2.ee02e522.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "465b9151d7051242417e",
+    "url": "/DigitalPortfolio/static/js/main.fcd3ffac.chunk.js"
+  },
+  {
+    "revision": "6a3a07957a8f5a169ddb",
+    "url": "/DigitalPortfolio/static/js/runtime-main.1f9a207a.js"
+  },
+  {
+    "revision": "03590f655995fb19029330c873d0929b",
+    "url": "/DigitalPortfolio/static/media/ArpitSirasaoResume.03590f65.pdf"
+  },
+  {
+    "revision": "0e42ae6e3fe2ca6b4eb0c88af6221b40",
+    "url": "/DigitalPortfolio/static/media/ProjectAssignment1.0e42ae6e.png"
+  },
+  {
+    "revision": "49e8a2ca3b9f853bd651114c1201767b",
+    "url": "/DigitalPortfolio/static/media/ProjectAssignment2.49e8a2ca.png"
+  },
+  {
+    "revision": "00d2091ffeac5a0bd8f0509ae7e4e905",
+    "url": "/DigitalPortfolio/static/media/ProjectLibra1.00d2091f.png"
+  },
+  {
+    "revision": "03b4e688c831a2c4a179c55a6c8619f2",
+    "url": "/DigitalPortfolio/static/media/ProjectLibra2.03b4e688.png"
+  },
+  {
+    "revision": "b4c270df1fe39a6ab0f1ec0714a6dbe1",
+    "url": "/DigitalPortfolio/static/media/ProjectMyPortfolio1.b4c270df.png"
+  },
+  {
+    "revision": "b630e013c081c723fc5f60785b22b73b",
+    "url": "/DigitalPortfolio/static/media/ProjectMyPortfolio2.b630e013.png"
+  },
+  {
+    "revision": "41d50582e948a78dd1357cdef61b6a52",
+    "url": "/DigitalPortfolio/static/media/arpit_logo.41d50582.jpeg"
+  },
+  {
+    "revision": "f4fbf9a7a62ddaa1240ee5896cef2c59",
+    "url": "/DigitalPortfolio/static/media/logo.f4fbf9a7.jpg"
+  },
+  {
+    "revision": "6dc09719ce0f7567edc9b6e66e5926e8",
+    "url": "/DigitalPortfolio/static/media/portfolio_bg.6dc09719.jpeg"
+  },
+  {
+    "revision": "7fc9b4380dd2d60871b67204687931fe",
+    "url": "/DigitalPortfolio/static/media/resume_bg.7fc9b438.jpg"
+  }
+]);
